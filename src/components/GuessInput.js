6cff611846e36b8648f45e9234f6c771d5c8d76c
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-11-30 05:01:25 -0300

    v6 input chute funcionando, faltando finalizar jogo

--- src/components/GuessInput.js
+++ src/components/GuessInput.js
@@ -1,15 +1,26 @@
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 
 export default function GuessInput(props) {
-  return(
-    <>
-    <span>Já sei a palavra!</span>
-    <Chutar></Chutar>
-    <button onClick={() => {if(Chutar.innerHTML === props.chosenWord.join){console.log("yay")}else{console.log("perdeu tio")}}}>Chutar</button>
-    </>
+  const [chute, setChute] = useState("");
+  
+  
+  function chutar() {
+    if (chute === props.chosenWord) {
+      console.log(chute, props.chosenWord, "yay");
+    } else {
+      console.log(chute, props.chosenWord, "perdeu");
+    }
+  }
+  return (
+    <ChuteContainer>
+      <span>Já sei a palavra!</span>
+      <Chutar onChange={(event)=>setChute(event.target.value.toLowerCase())}></Chutar>
+      <button onClick={chutar}>Chutar</button>
+    </ChuteContainer>
   );
 }
 
-const Chutar = styled.input`
-  
-`;
+const Chutar = styled.input``;
+
+const ChuteContainer = styled.div``;
